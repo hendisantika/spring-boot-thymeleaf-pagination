32767e02b94462089bc1b6e714f3952a92ae93dd
hendisantika <hendisantika@yahoo.co.id>
2023-03-02 12:22:02 +0700

    #1 - Initial commit ✏️📗:octocat:

---
+++ src/main/java/com/hendisantika/springbootthymeleafpagination/SpringBootThymeleafPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootthymeleafpagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafPaginationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThymeleafPaginationApplication.class, args);
+    }
+
+}
