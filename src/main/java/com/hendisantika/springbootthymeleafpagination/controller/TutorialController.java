fb7d6f15e50a4b36dcc8d141f11af5b4db516877
hendisantika <hendisantika@yahoo.co.id>
2023-03-02 12:28:19 +0700

    #8 - Add addTutorial in TutorialController ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootthymeleafpagination/controller/TutorialController.java
+++ src/main/java/com/hendisantika/springbootthymeleafpagination/controller/TutorialController.java
@@ -61,4 +61,15 @@ public class TutorialController {
 
         return "tutorials";
     }
+
+    @GetMapping("/new")
+    public String addTutorial(Model model) {
+        Tutorial tutorial = new Tutorial();
+        tutorial.setPublished(true);
+
+        model.addAttribute("tutorial", tutorial);
+        model.addAttribute("pageTitle", "Create new Tutorial");
+
+        return "tutorial-form";
+    }
 }
