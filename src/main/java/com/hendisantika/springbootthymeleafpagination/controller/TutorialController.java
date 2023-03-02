bb528f5cc82a11f49d4ecc5660ea95fb13031f48
hendisantika <hendisantika@yahoo.co.id>
2023-03-02 12:29:03 +0700

    #10 - Add editTutorial in TutorialController ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootthymeleafpagination/controller/TutorialController.java
+++ src/main/java/com/hendisantika/springbootthymeleafpagination/controller/TutorialController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -86,5 +87,21 @@ public class TutorialController {
         }
 
         return "redirect:/tutorials";
+    }
+
+    @GetMapping("/{id}")
+    public String editTutorial(@PathVariable("id") Integer id, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            Tutorial tutorial = tutorialRepository.findById(id).get();
+
+            model.addAttribute("tutorial", tutorial);
+            model.addAttribute("pageTitle", "Edit Tutorial (ID: " + id + ")");
+
+            return "tutorial-form";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("message", e.getMessage());
+
+            return "redirect:/tutorials";
+        }
     }
 }
