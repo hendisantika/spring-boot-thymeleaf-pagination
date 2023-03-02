56d4ef5644a5bb79a4cc796755c517ed54b27014
hendisantika <hendisantika@yahoo.co.id>
2023-03-02 12:27:51 +0700

    #7 - Add getAllTutorials in TutorialController ✏️📗:octocat:

--- src/main/java/com/hendisantika/springbootthymeleafpagination/controller/TutorialController.java
+++ src/main/java/com/hendisantika/springbootthymeleafpagination/controller/TutorialController.java
@@ -1,8 +1,19 @@
 package com.hendisantika.springbootthymeleafpagination.controller;
 
+import com.hendisantika.springbootthymeleafpagination.entity.Tutorial;
 import com.hendisantika.springbootthymeleafpagination.repository.TutorialRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,8 +26,39 @@ import org.springframework.stereotype.Controller;
  * To change this template use File | Settings | File Templates.
  */
 @Controller
+@RequestMapping("/tutorials")
 @RequiredArgsConstructor
 public class TutorialController {
 
     private final TutorialRepository tutorialRepository;
+
+    @GetMapping
+    public String getAllTutorials(Model model, @RequestParam(required = false) String keyword,
+                                  @RequestParam(defaultValue = "1") int page,
+                                  @RequestParam(defaultValue = "3") int size) {
+        try {
+            List<Tutorial> tutorials = new ArrayList<Tutorial>();
+            Pageable paging = PageRequest.of(page - 1, size);
+
+            Page<Tutorial> pageTuts;
+            if (keyword == null) {
+                pageTuts = tutorialRepository.findAll(paging);
+            } else {
+                pageTuts = tutorialRepository.findByTitleContainingIgnoreCase(keyword, paging);
+                model.addAttribute("keyword", keyword);
+            }
+
+            tutorials = pageTuts.getContent();
+
+            model.addAttribute("tutorials", tutorials);
+            model.addAttribute("currentPage", pageTuts.getNumber() + 1);
+            model.addAttribute("totalItems", pageTuts.getTotalElements());
+            model.addAttribute("totalPages", pageTuts.getTotalPages());
+            model.addAttribute("pageSize", size);
+        } catch (Exception e) {
+            model.addAttribute("message", e.getMessage());
+        }
+
+        return "tutorials";
+    }
 }
